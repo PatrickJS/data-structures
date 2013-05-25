09d09f2062e587e87fa3c2dfe05a65c0bb9e3c7b
gdi2290 <github@gdi2290.com>
2013-05-25 11:18:20 -0700

    finish Queue

---
+++ Queue/queue.js
@@ -0,0 +1,19 @@
+var Queue = function() {
+  this.storage = {};
+  this.size = 0;
+  this.front = 0;
+};
+
+Queue.prototype.add = function(value) {
+  this.storage[this.size] = value;
+  ++this.size;
+};
+Queue.prototype.remove = function() {
+  var temp = this.storage[this.front];
+  delete this.storage[this.front];
+  ++this.front;
+  return temp;
+};
+Queue.prototype.length = function(value) {
+  return this.size - this.front;
+};
