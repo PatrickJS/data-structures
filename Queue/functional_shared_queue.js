af3eb5555e76cb4906993e6a0fb6aac7137ed7b5
gdi2290 <github@gdi2290.com>
2013-08-03 14:29:28 -0700

    create methods and extend shared methods

--- Queue/functional_shared_queue.js
+++ Queue/functional_shared_queue.js
@@ -1,2 +1,9 @@
 var makeQueue = function(){
+  var methods = {};
+  methods.storage = {};
+  methods.front = 0;
+  methods.queueSize = 0;
+  _.extend(methods, queueMethods);
+
+  return methods;
 };
