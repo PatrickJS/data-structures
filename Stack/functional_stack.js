00f514dcb21a7ce3250353df2c1845d9c35afedf
gdi2290 <github@gdi2290.com>
2013-08-03 12:23:31 -0700

    add methods to fn stack

--- Stack/functional_stack.js
+++ Stack/functional_stack.js
@@ -2,4 +2,15 @@ var makeStack = function(){
   var storage = {},
       size = 0,
       stack = {};
+
+  stack.push = function(value){
+  };
+
+  stack.pop = function(){
+  };
+
+  stack.size = function(){
+  };
+
+  return stack;
 };
