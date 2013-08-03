9cb8cf7667c0cfb260e1834725fcd7f668939fb4
gdi2290 <github@gdi2290.com>
2013-08-03 12:22:04 -0700

    add Helper functions for hashtables

--- HashTable/hashtable.js
+++ HashTable/hashtable.js
@@ -1,3 +1,37 @@
+// Start Helper functions
+var makeLimitedArray = function(limit) {
+  var storage = [];
+
+  var limitedArray = {};
+  limitedArray.get = function(index){
+    checkLimit(index);
+    return storage[index];
+  };
+  limitedArray.set = function(index, value) {
+    checkLimit(index);
+    storage[index] = value;
+  };
+
+  var checkLimit = function(index){
+    if(typeof index !== 'number'){ throw new Error('setter requires a numeric index for its first argument'); }
+    if(limit <= index){ throw new Error('Error trying to access an over-the-limit index'); }
+  };
+
+  return limitedArray;
+};
+var getIndexBelowMaxForKey = function(str, max){
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
+// End Helper functions
+
+
+
 var HashTable = function(limit){
   this._limit = limit || 8;
 
@@ -93,3 +127,5 @@ HashTable.prototype.length = function() {
   }
   return size;
 };
+
+
