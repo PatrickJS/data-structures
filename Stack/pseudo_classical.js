2293ce83e6d5b895f3f97609ad6edb5d0b2c8f57
gdi2290 <github@gdi2290.com>
2013-08-03 13:13:11 -0700

    extend prototype of Stack with methods: push, pop, size

--- Stack/pseudo_classical.js
+++ Stack/pseudo_classical.js
@@ -2,3 +2,13 @@ var Stack = function(){
   this.storage = {};
   this.stackSize = 0;
 };
+
+
+Stack.prototype.push = function(value){
+};
+
+Stack.prototype.pop = function(){
+};
+
+Stack.prototype.size = function(){
+};
