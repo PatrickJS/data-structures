9e21edc30fe99274166138fbe9b4e1eb6086a549
gdi2290 <github@gdi2290.com>
2013-08-04 15:08:39 -0700

    add a unique key method to each method

--- Set/set.js
+++ Set/set.js
@@ -6,14 +6,17 @@ var makeSet = function(){
 
 
 makeSet.prototype.add = function(value){
+  value = this.generateKey(value);
   this._storage[value] = value;
 };
 
 makeSet.prototype.contains = function(value){
+  value = this.generateKey(value);
   return this._storage[value] === value;
 };
 
 makeSet.prototype.remove = function(value){
+  value = this.generateKey(value);
   var temp = this._storage[value];
   return (delete this._storage[value]) && temp;
 };
