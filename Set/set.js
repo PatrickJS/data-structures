ac5ba61e0b40b0c481bf78687f263fe9850d6cc4
gdi2290 <github@gdi2290.com>
2013-08-04 15:07:06 -0700

    init js set

--- Set/set.js
+++ Set/set.js
@@ -3,36 +3,3 @@ var makeSet = function(){
   set._storage = {};
   return set;
 };
-
-makeSet.prototype.generateKey = function (value) {
-  var type = typeof value;
-  if (type === 'function') {
-    value = value.toString();
-  }
-  else if(type === 'undefined') {
-    return 'undefined';
-  }
-  //NaN
-  else if(type === 'number' && !value && value !==0) {
-    return 'NaN';
-  }
-  else {
-    value = JSON.stringify(value);
-  }
-  return value;
-};
-
-makeSet.prototype.add = function(value){
-  value = this.generateKey(value);
-  this._storage[value] = true;
-};
-
-makeSet.prototype.contains = function(value){
-  value = this.generateKey(value);
-  return !!this._storage[value];
-};
-
-makeSet.prototype.remove = function(value){
-  value = this.generateKey(value);
-  delete this._storage[value];
-};
